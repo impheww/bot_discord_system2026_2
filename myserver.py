2a705f2635326a905640993c4fa290ba7b3a8718
impheww <dalph3103@gmail.com>
2026-02-18 10:29:49 +0700

    Update myserver.py

--- myserver.py
+++ myserver.py
@@ -6,12 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bot is alive"
+    return "Bot is alive!"
 
 def run():
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
 def server_on():
-    t = Thread(target=run)
-    t.start()
+    Thread(target=run).start()
